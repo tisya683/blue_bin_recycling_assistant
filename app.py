9bf389a8f349674609c1444574400eb1edc258dd
tisya683 <74233811+tisya683@users.noreply.github.com>
2026-06-29 17:23:41 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import gradio as gr
+from predict import predict_img
+
+with gr.Blocks() as demo:
+
+    gr.Markdown("# Blue Bin Recycling Assistant")
+
+    image = gr.Image(type="pil")
+
+    is_clean = gr.Checkbox(label="Clean?")
+    is_dry = gr.Checkbox(label="Dry?")
+    is_rigid = gr.Checkbox(label="Rigid?")
+
+    output = gr.JSON()
+
+    submit = gr.Button("Classify")
+
+    submit.click(
+        fn=predict_img,
+        inputs=[image, is_clean, is_dry, is_rigid],
+        outputs=output
+    )
+
+demo.launch()
